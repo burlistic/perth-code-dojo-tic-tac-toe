bdd56f0717ab78fd2c9b9ea889cfd3644a884008
burlistic <burlistic@yahoo.co.uk>
2015-07-24 21:15:36 +0800

    Added class and first test method

--- TicTacToe.scala
+++ TicTacToe.scala
@@ -1,22 +1,44 @@
-//class TicTacToeGame {
+class TicTacToeGame {
 
 // constructor? empty string?
 
 //var board
 
-def MakeAMove {
-	 println("welcome to ticTacToe!")
+	def MakeAMove {
+		 println("welcome to ticTacToe!")
 
-	 //println("input: " + args)
+		 //println("input: " + args)
 
-	 var board = ""
-	 board = arg(0)
+		 var board = ""
+		 board = args(0)
 
-	 // for(i <- 0 until args.length) {
-	 //  	println(args(i))
-	 // }
+		 // for(i <- 0 until args.length) {
+		 //  	println(args(i))
+		 // }
 
-	 println("board: " + board)
+		 println("board: " + board)
+	}	
+
+
+	// TODO - move to test lib
+    def Test(input:Int, expected:String) = {
+
+	val output = Convert(input)
+
+	if(output != expected)
+	{
+		println("Test failed. Input " + input + 
+			" Output: " + output + 
+			" Expected: " + expected)
+
+
+	}
+
+	println("Test passed. Input " + input + " Output: " + output)
+    }
+
 }
 
-MakeAMove
+var ticTacToeGame = new TicTacToeGame()
+
+ticTacToeGame.MakeAMove(X_________, X_________)
