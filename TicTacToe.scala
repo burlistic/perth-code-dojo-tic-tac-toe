6d77ecc144aa6f9260cdba23f67c53d8d3d5c248
burlistic <burlistic@yahoo.co.uk>
2015-07-24 22:00:41 +0800

    Second test passes. Fixed error. Input used instead of args

--- TicTacToe.scala
+++ TicTacToe.scala
@@ -7,14 +7,19 @@ class TicTacToeGame {
 	def MakeAMove(input:String): String = {
 		 println("welcome to ticTacToe!")
 
-		 //println("input: " + args)
+		 //println("input: " + args(0))
 
 		 var board = ""
-		 board = args(0)
+		 board = input
 
 		 // for(i <- 0 until args.length) {
 		 //  	println(args(i))
 		 // }
+
+		 if(board.contains("xxx"))
+		 {
+		 	return "x wins! " + board
+		 }
 
 		 println("board: " + board)
 
@@ -28,10 +33,7 @@ class TicTacToeGame {
 
 class TestLib {
 
-// TODO - move to test lib
     def Test(input:String, expected:String):Boolean = {
-
-	//val output = Convert(input)
 
 	if(input != expected)
 	{
@@ -56,7 +58,12 @@ var testLib = new TestLib();
 // Board returns state once a move has been made
 
 var output1 = ticTacToeGame.MakeAMove("x_________")
-testLib.Test("x_________", output1); 
+testLib.Test(output1, "x_________"); 
+
+// Simple win
+
+var output2 = ticTacToeGame.MakeAMove("xxx_______")
+testLib.Test(output2, "x wins! xxx_______"); 
 
 // TODO - uppcase getting converted to lower case?
 
