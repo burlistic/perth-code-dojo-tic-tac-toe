32e511b97ab5d12bc7510ec3d5305046950fe9de
burlistic <burlistic@yahoo.co.uk>
2015-07-24 21:15:07 +0800

    Initial commit - method that takes input and prints it

---
+++ TicTacToe.scala
@@ -0,0 +1,22 @@
+//class TicTacToeGame {
+
+// constructor? empty string?
+
+//var board
+
+def MakeAMove {
+	 println("welcome to ticTacToe!")
+
+	 //println("input: " + args)
+
+	 var board = ""
+	 board = arg(0)
+
+	 // for(i <- 0 until args.length) {
+	 //  	println(args(i))
+	 // }
+
+	 println("board: " + board)
+}
+
+MakeAMove
